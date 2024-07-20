1440e0e69cad862847439ce1e7199ec2f9633025
Code masters <pankajhadole24@gmail.com>
2024-07-21 01:59:29 +0530

    add packages

--- js/scripts.js
+++ js/scripts.js
@@ -46,3 +46,44 @@ document.getElementById('mission-btn').addEventListener('mouseleave', function()
     document.getElementById('mission-overlay').style.display = 'none';
   }, 300);
 });
+
+
+document.getElementById('viewPlanButton').addEventListener('mouseenter', function() {
+  document.getElementById('packagesGrid').classList.remove('hidden');
+  document.getElementById('packagesGrid').classList.add('visible');
+});
+document.addEventListener('DOMContentLoaded', () => {
+  const viewPlanButton = document.getElementById('viewPlanButton');
+  const packagesGrid = document.getElementById('packagesGrid');
+
+  viewPlanButton.addEventListener('click', () => {
+    // Toggle visibility for both mobile and desktop views
+    packagesGrid.classList.toggle('visible');
+    
+    // Scroll to the packages grid in mobile view
+    if (window.innerWidth <= 767) {
+      if (packagesGrid.classList.contains('visible')) {
+        packagesGrid.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }
+    // Scroll to the packages grid in desktop view
+    else {
+      if (packagesGrid.classList.contains('visible')) {
+        packagesGrid.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }
+  });
+});
+
+document.addEventListener('scroll', () => {
+  const imageSection = document.querySelector('.lg:flex.relative');
+  const message = document.getElementById('message');
+  const rect = imageSection.getBoundingClientRect();
+  
+  // Adjust the threshold value as needed
+  if (rect.top < window.innerHeight && rect.bottom >= 0) {
+      message.classList.remove('hidden');
+  } else {
+      message.classList.add('hidden');
+  }
+});
