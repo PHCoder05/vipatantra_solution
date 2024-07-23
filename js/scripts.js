a99cb7e140b5cc71cb6f7b5a28388ba56baa9bd6
Krishna Patil <krishnapatil6446@gmail.com>
2024-07-23 16:22:17 +0530

    updated some changes

--- js/scripts.js
+++ js/scripts.js
@@ -47,31 +47,3 @@ document.getElementById('mission-btn').addEventListener('mouseleave', function()
   }, 300);
 });
 
-
-document.getElementById('viewPlanButton').addEventListener('mouseenter', function() {
-  document.getElementById('packagesGrid').classList.remove('hidden');
-  document.getElementById('packagesGrid').classList.add('visible');
-});
-document.addEventListener('DOMContentLoaded', () => {
-  const viewPlanButton = document.getElementById('viewPlanButton');
-  const packagesGrid = document.getElementById('packagesGrid');
-
-  viewPlanButton.addEventListener('click', () => {
-    // Toggle visibility for both mobile and desktop views
-    packagesGrid.classList.toggle('visible');
-    
-    // Scroll to the packages grid in mobile view
-    if (window.innerWidth <= 767) {
-      if (packagesGrid.classList.contains('visible')) {
-        packagesGrid.scrollIntoView({ behavior: 'smooth', block: 'start' });
-      }
-    }
-    // Scroll to the packages grid in desktop view
-    else {
-      if (packagesGrid.classList.contains('visible')) {
-        packagesGrid.scrollIntoView({ behavior: 'smooth', block: 'start' });
-      }
-    }
-  });
-});
-
